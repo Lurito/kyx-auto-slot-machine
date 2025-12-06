e69f89916a776b489ab867bcfdc138c4639ae632
Ayrzo <ayrzo@lurito.cn>
2025-12-06 17:50:11 +0800

    feat: 添加更新 URL 等元信息

--- auto-slot-machine.js
+++ auto-slot-machine.js
@@ -2,6 +2,10 @@
 // @name         Auto Slot Machine
 // @version      1.0.1
 // @description  在页面右上角添加一个悬浮按钮，每 6 秒自动点击一次 button#spinButton 来进行抽奖
+// @homepage     https://github.com/Lurito/kyx-auto-slot-machine
+// @updateURL    https://cdn.jsdelivr.net/gh/Lurito/kyx-auto-slot-machine/auto-slot-machine.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/Lurito/kyx-auto-slot-machine/auto-slot-machine.js
+// @supportURL   https://github.com/Lurito/kyx-auto-slot-machine/issues
 // @match        https://quota.kyx03.de/
 // @match        https://quota.wpgzs.top/
 // @grant        none
